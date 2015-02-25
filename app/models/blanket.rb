75279c37102b5dae88d9992b34f63bf78c3f866e
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-24 21:26:00 -0700

    Added validations to Blanket model, and HTML to display them in the form partial

--- app/models/blanket.rb
+++ app/models/blanket.rb
@@ -1,5 +1,8 @@
 class Blanket < ActiveRecord::Base
-  
 
+  validates :name, presence: true
+  validates :color, presence: true
+  validates :fabric, presence: true
+  validates :weight, presence: true
 
 end
